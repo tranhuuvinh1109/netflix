4111ecdcc94f56206a092c6e8a8a837d517d34fd
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-07-17 00:19:19 +0700

    setup prisma and handle register and sigin use next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,63 @@
+import NextAuth, { AuthOptions } from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import prismadb from '../../../../lib/prismadb'
+import { compare } from 'bcrypt'
+
+export const authOptions: AuthOptions = {
+	providers: [
+		Credentials({
+			id: 'credentials',
+			name: 'Credentials',
+			credentials: {
+				email: {
+					label: 'Email',
+					type: 'text',
+				},
+				password: {
+					label: 'Password',
+					type: 'password',
+				},
+			},
+			async authorize (credentials) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error('Email and password required');
+				}
+				const user = await prismadb.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				});
+
+				if (!user || !user.hashedPassword) {
+					throw new Error('Email does not exist');
+				}
+
+				const isCorrectPassword = await compare(
+					credentials.password,
+					user.hashedPassword
+				);
+
+				if (!isCorrectPassword) {
+					throw new Error('Incorrect password');
+				}
+				console.log(12, user)
+				return user;
+			},
+		}),
+	],
+	pages: {
+		signIn: '/auth',
+	},
+	debug: process.env.NODE_ENV === 'development',
+	session: {
+		strategy: 'jwt',
+	},
+	jwt: {
+		secret: process.env.NEXTAUTH_JWT_SECRET,
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+};
+
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST } 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		DATABASE_URL: "mongodb+srv://tranhuuvinh1109:vinh1109@cluster0.fg9i8xq.mongodb.net/test",
+		NEXTAUTH_JWT_SECRET: "NEXT-JWT-SECRET",
+		NEXTAUTH_SECRET: "NEXT-SECRET"
+	},
+}
 
 module.exports = nextConfig
