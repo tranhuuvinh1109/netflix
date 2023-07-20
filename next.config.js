2ed2cf5590efbb45126dc2cedce9ea3ff50ccee2
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-07-20 11:34:03 +0700

    change env value

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
 		NEXTAUTH_SECRET: "NEXT-SECRET",
 		GITHUB_ID: "d2ffa7b1f04eb2e4905c",
 		GITHUB_SECRET: "361b534de912e60930f31cdebe333409cb7aa15b ",
-		GOOGLE_CLIENT_ID: "329648410405-5goedjjncmhbbc4fldvms6ustjlad99u.apps.googleusercontent.com",
-		GOOGLE_CLIENT_SECRET: "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",
+		// GOOGLE_CLIENT_ID: "329648410405-5goedjjncmhbbc4fldvms6ustjlad99u.apps.googleusercontent.com",
+		// GOOGLE_CLIENT_SECRET: "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",
+		GOOGLE_CLIENT_ID: "329648410405-4r4oaq6f80ost7eghapuigdijstk8k8r.apps.googleusercontent.com",
+		GOOGLE_CLIENT_SECRET: "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",
 	},
 }
 
