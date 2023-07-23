8de4a80b3e230044162c450428ef7d622dff68b8
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-07-23 16:11:23 +0700

    change redirect

--- app/components/Billboard/index.tsx
+++ app/components/Billboard/index.tsx
@@ -6,26 +6,28 @@ import { useBillboard, useInfoModalStore } from '@/app/hooks';
 
 
 const Billboard: React.FC = () => {
-	const { data } = useBillboard()
+	const { data, isLoading } = useBillboard()
 	const { openModal } = useInfoModalStore();
 	const handleOpenModal = useCallback(() => {
 		openModal(data?.id);
 	}, [openModal, data?.id]);
 	return (
 		<div className="relative h-[56.25vw]">
-			<video poster={data?.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={data?.videoUrl}></video>
-			<div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
-				<p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
-					{data?.title}
-				</p>
-				<p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
-					{data?.description}
-				</p>
-				<div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-					<PlayButton movieId={data?.id} />
-					<button
-						onClick={handleOpenModal}
-						className="
+			{
+				!isLoading && <>
+					<video poster={data?.thumbnailUrl} className="w-full h-[56.25vw] object-cover brightness-[60%] transition duration-500" autoPlay muted loop src={data?.videoUrl}></video>
+					<div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+						<p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+							{data?.title}
+						</p>
+						<p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl">
+							{data?.description}
+						</p>
+						<div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+							<PlayButton movieId={data?.id} />
+							<button
+								onClick={handleOpenModal}
+								className="
             bg-white
             text-white
               bg-opacity-30 
@@ -41,12 +43,14 @@ const Billboard: React.FC = () => {
               hover:bg-opacity-20
               transition
             "
-					>
-						<InformationCircleIcon className="w-4 md:w-7 mr-1" />
-						More Info
-					</button>
-				</div>
-			</div>
+							>
+								<InformationCircleIcon className="w-4 md:w-7 mr-1" />
+								More Info
+							</button>
+						</div>
+					</div>
+				</>
+			}
 		</div>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,46 @@
 'use client';
 // import { NextPageContext } from 'next'
-// import { getSession } from 'next-auth/react'
+import React, { } from 'react'
+import { useSession } from 'next-auth/react'
 import { Billboard, InfoModal, MovieList, Navbar } from './components';
-import { useCurrentUser, useFavorites, useInfoModalStore, useMovieList } from './hooks';
-// export async function getServerSideProps (context: NextPageContext) {
-//   const session = await getSession(context)
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: '/auth',
-//         permanent: false
-//       }
-//     }
-//   }
+import { useFavorites, useInfoModalStore, useMovieList } from './hooks';
+import { redirect } from 'next/navigation';
 
-//   return {
-//     props: {}
-//   }
-// }
 export default function Home () {
-  const { isOpen, closeModal } = useInfoModalStore();
+  const { } = useSession({
+    required: true,
+    onUnauthenticated () {
+      redirect('/auth')
+    }
+  })
 
-  const { data: movies = [] } = useMovieList();
-  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
+  const { data: movies = [], isLoading: isLoadingMoive } = useMovieList();
+  const { data: favorites = [], isLoading: isLoadingFavorites } = useFavorites();
   return (
     <main className=''>
       <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className='pt-40'>
-        <p className='text-4xl text-white'>
-          Movie list
-        </p>
-        <MovieList title="Trending Now" data={movies} />
-        <p className='text-4xl text-white'>
-          Favorites list
-        </p>
-        <MovieList title="Trending Now" data={favorites} />
+
+        {
+          isLoadingMoive === false && movies && <>
+            <p className='text-4xl text-white'>
+              Movie list
+            </p>
+            <MovieList title="Trending Now" data={movies} />
+          </>
+        }
+        {
+          isLoadingFavorites === false && favorites && <>
+            <p className='text-4xl text-white'>
+              Favorites list
+            </p>
+            <MovieList title="Trending Now" data={favorites} />
+          </>
+        }
+
       </div>
 
     </main>
